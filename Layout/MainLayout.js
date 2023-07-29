c49bfbf918fea027ee9350344c342eb12c358d9b
Harsh Tiwari <124431898+Harsh21it@users.noreply.github.com>
2023-07-29 19:56:13 +0530

    Add files via upload

--- Layout/MainLayout.js
+++ Layout/MainLayout.js
@@ -1,13 +1,11 @@
 import React from 'react'
 import Navbar from '../Components/Navbar';
 import Footer from '../Components/Footer';
-import VideoPlay from '../Components/Video'
 
 function MainLayout({children}) {
   return (
     <div>
         <Navbar />
-        <VideoPlay />
         <div>{children}</div>
         <Footer />
     </div>
